88df5d115328e1f35c74eb30213222b67793d863
Michael T. <66504185+michaeltukdev@users.noreply.github.com>
2024-04-07 03:45:40 +0100

    Base files
    This includes the main app layout.
    This includes TALL Stack setup.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{  App::currentLocale(); }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ env('APP_NAME') }} - @yield('title')</title>
+
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=montserrat" rel="stylesheet" />
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', "Home")
+
+@section('content')
+abcc
+@endsection
