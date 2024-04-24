63df3b2b593adf4517051a430958116a2300bf83
Michael T. <66504185+michaeltukdev@users.noreply.github.com>
2024-04-24 08:30:02 +0100

    update: Fixing dropdown size & responsive
    - Gave it a fixed sized
    - Made it start on left not right when larger than mobile.

--- resources/views/livewire/partials/dropdown.blade.php
+++ resources/views/livewire/partials/dropdown.blade.php
@@ -1,18 +1,23 @@
-<div x-show="open" x-on:click.away="open = false" x-transition class="absolute right-0 w-full max-w-lg p-4 border rounded-lg shadow-lg bg-container border-input-border top-10">
+<div x-show="open" x-on:click.away="open = false" x-transition
+    class="absolute left-0 md:left-auto md:right-0 w-[150px] p-4 border rounded-lg shadow-lg bg-container border-input-border top-10">
     <ul class="w-full space-y-3">
         <li>
-            <a href="#" class="block text-sm transition text-text-secondary hover:text-text-primary" wire:navigate>Profile</a>
+            <a href="#" class="block text-sm transition text-text-secondary hover:text-text-primary"
+                wire:navigate>Profile</a>
         </li>
         <li>
-            <a href="#" class="block text-sm transition text-text-secondary hover:text-text-primary" wire:navigate>Settings</a>
+            <a href="#" class="block text-sm transition text-text-secondary hover:text-text-primary"
+                wire:navigate>Settings</a>
         </li>
         @can('panel access')
-        <li>
-            <a href="{{ route('panel') }}" class="block text-sm transition text-text-secondary hover:text-text-primary" wire:navigate>Control Panel</a>
-        </li>
+            <li>
+                <a href="{{ route('panel') }}" class="block text-sm transition text-text-secondary hover:text-text-primary"
+                    wire:navigate>Control Panel</a>
+            </li>
         @endcan
         <li>
-            <a href="{{ route('logout') }}" class="block text-sm text-red-300 transition hover:text-red-400" wire:navigate>Logout</a>
+            <a href="{{ route('logout') }}" class="block text-sm text-red-300 transition hover:text-red-400"
+                wire:navigate>Logout</a>
         </li>
     </ul>
 </div>
